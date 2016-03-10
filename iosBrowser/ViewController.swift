3e3d9d090df51ab4107c362e3fba0279a0a982b0
kuwako <kuwako@leverages.jp>
2016-03-10 20:02:52 +0900

    Initial Commit

---
+++ iosBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosBrowser
+//
+//  Created by 桑古　昌輝 on 2016/03/10.
+//  Copyright © 2016年 桑古　昌輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
